a2cd6ca6181f1541706c7ed5a00333e6f6084f04
GuoHaoLan <coopticks@hotmail.com>
2022-08-03 22:54:29 +0800

    Add SRT Converter

---
+++ go.mod
@@ -0,0 +1,2 @@
+module github.com/MeteorsLiu/goSRT
+go 1.18
---
+++ srt/srt.go
@@ -0,0 +1,68 @@
+package srt
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type SRT struct {
+	builder strings.Builder
+	count   byte
+}
+
+// formatTime function converts the internal time to SubRip Time
+// for example 137.99 -> 00:02:17,990
+func formatTime(internalTime string) (string, error) {
+	_parseTime := strings.Split(internalTime, ".")
+	if len(_parseTime) != 2 {
+		return "", errors.New("unknown timestamp")
+	}
+	intTime, err := strconv.ParseInt(_parseTime[0], 10, 64)
+	if err != nil {
+		return "", err
+	}
+	milliTime := _parseTime[1]
+	// Cut off the string if it's overflow.
+	if len(milliTime) > 3 {
+		milliTime = milliTime[:3]
+	}
+	// Padding to the length 3
+	for len(milliTime) < 3 {
+		milliTime += "0"
+	}
+
+	toConvertTime := time.Unix(intTime, 0)
+	return fmt.Sprintf("%02d:%02d:%02d,%s",
+		toConvertTime.Hour(),
+		toConvertTime.Minute(),
+		toConvertTime.Second(),
+		milliTime), nil
+}
+
+func New() *SRT {
+	return &SRT{
+		count: byte(1),
+	}
+}
+
+func (s *SRT) Append(start, end, content string) {
+	s.builder.WriteByte(s.count)
+	s.builder.WriteString("\n")
+	start_, err1 := formatTime(start)
+	_end, err2 := formatTime(end)
+	if err1 != nil || err2 != nil {
+		return
+	}
+	s.builder.WriteString(start_ + " --> " + _end)
+	s.builder.WriteString("\n")
+	s.builder.WriteString(content)
+	s.builder.WriteString("\n")
+	s.count++
+}
+
+func (s *SRT) String() string {
+	return s.builder.String()
+}
