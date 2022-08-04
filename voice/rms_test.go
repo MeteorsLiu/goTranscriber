9dc3307ca9200a53038d7995b321fcd5dedee47f
GuoHaoLan <coopticks@hotmail.com>
2022-08-04 20:57:28 +0800

    Fix rms_test

--- voice/rms_test.go
+++ voice/rms_test.go
@@ -11,13 +11,15 @@ func TestRMS(t *testing.T) {
 	file, _ := os.Open("/home/nfs/py/GVRD-94/1.wav")
 	reader := wav.NewReader(file)
 	defer file.Close()
+	var sum float64
 	for {
 		samples, err := reader.ReadSamples(4096)
 		if err == io.EOF {
 			break
 		}
-		t.Log(rms(samples))
+		sum += rms(samples)
 
 	}
+	t.Log(sum/4096.0)
 
 }
