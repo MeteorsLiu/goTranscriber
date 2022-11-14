1d937d6ed5dcd0ddb24ec2cdf2d1e3ff8d4c3689
GuoHaoLan <coopticks@hotmail.com>
2022-11-14 22:20:12 +0800

    Add help

--- main.go
+++ main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
-	"errors"
+	"flag"
 	"fmt"
 	"os"
-	"path/filepath"
+
+	"github.com/MeteorsLiu/goSRT/transcribe"
+)
+
+var (
+	lang     string
+	filename string
+	isHelp   int
 )
 
 func main() {
-	m, _ := filepath.Glob("/home/nfs/py/[A-Z]*/[A-Z]*.mp4")
-	for _, ms := range m {
-		if _, err := os.Stat(getSrtName(ms)); errors.Is(err, os.ErrNotExist) {
-			fmt.Println(ms)
+	flag.IntVar(&isHelp, "help", 1, "Show me Help(使用帮助)")
+	flag.StringVar(&lang, "lang", "", "Source Video Language(源文件语言)")
+	flag.StringVar(&filename, "file", "", "Source Video(原视频文件)")
+	flag.Parse()
 
-			DoVad("ja", ms)
+	if isHelp == 0 {
+		fmt.Println("具体使用方式：")
+		fmt.Println("./gotranscriber -file xxx.mp4(File to be transcribed. 需要听识的视频/音频文件) -lang ja(原视频文件语言缩写)")
+		fmt.Println("以下是语言简写")
+		for k, v := range transcribe.GetLangCode() {
+			fmt.Println(k, "->", v)
 		}
+		os.Exit(0)
 	}
+
+	DoVad(lang, filename)
+
 }
